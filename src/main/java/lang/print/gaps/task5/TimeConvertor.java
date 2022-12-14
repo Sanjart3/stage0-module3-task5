194647ae4214bfba22fa6b331c8c9cdfca66eb74
Sanjar Totliboyev <2110110@newuzbekistanuniversity.uz>
2022-12-14 16:30:24 +0500

    just

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -3,5 +3,6 @@ package lang.print.gaps.task5;
 public class TimeConvertor {
     public void convert(float minutes) {
         double seconds = minutes*60;
+        System.out.println(seconds);
     }
 }
